9caad50fde62763328d8b9fbd7c371ed39f47562
natewu <36091727+natewu@users.noreply.github.com>
2024-05-13 15:14:03 -0600

    remove bin track

--- obj/Debug/net8.0/blazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c2952c8571abd22c6e8d8540c6d19a8f7bf352")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
